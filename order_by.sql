e56c991a6a3a788efb3f6766c8879d6db75a2467
Crystal <crystal.m.deleon00@gmail.com>
2023-02-17 01:13:40 -0600

    order by done

--- order_by.sql
+++ order_by.sql
@@ -5,3 +5,7 @@ SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY 
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name DESC;
 SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
 SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
+
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
