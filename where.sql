af39a841383173332ebd842d075bb2391e74e55a
Crystal <crystal.m.deleon00@gmail.com>
2023-02-17 01:09:56 -0600

    where done

--- where.sql
+++ where.sql
@@ -7,4 +7,5 @@ SELECT * FROM employees WHERE last_name LIKE '%Q%';
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
